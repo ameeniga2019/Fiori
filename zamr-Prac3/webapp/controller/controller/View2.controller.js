8ffdc5091af2ad1d62cdd628311fffc5b679a54d
ameeniga2019 <ameeniga2019@gmail.com>
2021-08-03 15:46:23 +0530

    Table dynamic data display

--- zamr-Prac3/webapp/controller/controller/View2.controller.js
+++ zamr-Prac3/webapp/controller/controller/View2.controller.js
@@ -3,12 +3,12 @@ sap.ui.define([
 ], function(Controller) {
 	"use strict";
 
-	return Controller.extend("controller.view1", {
+	return Controller.extend("MyAppzamr-Prac3.controller.controller.View2", {
 
 		/**
 		 * Called when a controller is instantiated and its View controls (if available) are already created.
 		 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
-		 * @memberOf view.view1
+		 * @memberOf MyAppzamr-Prac3.controller.view.View2
 		 */
 		//	onInit: function() {
 		//
@@ -17,7 +17,7 @@ sap.ui.define([
 		/**
 		 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
 		 * (NOT before the first rendering! onInit() is used for that one!).
-		 * @memberOf view.view1
+		 * @memberOf MyAppzamr-Prac3.controller.view.View2
 		 */
 		//	onBeforeRendering: function() {
 		//
@@ -26,7 +26,7 @@ sap.ui.define([
 		/**
 		 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
 		 * This hook is the same one that SAPUI5 controls get after being rendered.
-		 * @memberOf view.view1
+		 * @memberOf MyAppzamr-Prac3.controller.view.View2
 		 */
 		//	onAfterRendering: function() {
 		//
@@ -34,7 +34,7 @@ sap.ui.define([
 
 		/**
 		 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
-		 * @memberOf view.view1
+		 * @memberOf MyAppzamr-Prac3.controller.view.View2
 		 */
 		//	onExit: function() {
 		//
